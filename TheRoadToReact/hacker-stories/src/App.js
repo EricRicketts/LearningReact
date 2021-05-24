d171953f54898bc63d5dada214ad9c3e98f559aa
EricRicketts <eric_ricketts@me.com>
2021-05-24 15:22:31 -0400

    JSX render an array

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-const heading = {
-  greeting: 'Hello',
-  title: 'React'
-}
-function getHeading(heading) {
-  return `${heading.greeting} ${heading.title}`;
-}
+const words = ['Foo', 'Bar', 'Fizz', 'Buzz']
+
 function App() {
   return (
     <div>
-      <h1>{getHeading(heading)}</h1>
+      <h1>Hello React</h1>
+      <ul>
+        {words.map((word,index) => <li key={index}>{word}</li>)}
+      </ul>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search"/>
     </div>
