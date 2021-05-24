ff11737f48d58d359fc0563a742dada533deeebf
EricRicketts <eric_ricketts@me.com>
2021-05-24 14:48:42 -0400

    JSX renders an object

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+};
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search"/>
     </div>
   );
 }
