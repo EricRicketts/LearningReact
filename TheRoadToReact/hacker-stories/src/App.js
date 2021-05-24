41253d5b75457d02e2ce5dce6ed050382356b77f
EricRicketts <eric_ricketts@me.com>
2021-05-24 15:24:54 -0400

    JSX render an array part two

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 const words = ['Foo', 'Bar', 'Fizz', 'Buzz']
-
+function getWords(words) {
+  return words.map((word, index) => <li key={index}>{word}</li>)
+}
 function App() {
   return (
     <div>
       <h1>Hello React</h1>
       <ul>
-        {words.map((word,index) => <li key={index}>{word}</li>)}
+        {getWords(words)}
       </ul>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search"/>
