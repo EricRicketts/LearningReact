7e7c63b6a3331fe8618b7ead00dc9845c425e315
EricRicketts <eric_ricketts@me.com>
2021-05-26 14:07:15 -0400

    use of props to pass data from one component to another

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -1,26 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    number_of_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    number_of_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
-const List = () =>
-  list.map(item => (
+const List = props =>
+  props.list.map(item => (
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
@@ -30,18 +12,41 @@ const List = () =>
       <span>{item.points}</span>
     </div>
   ));
-const App = () => (
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      number_of_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      number_of_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ];
+
+  const handleChange = event => {
+    console.log(event);
+  }
+  return (
     <div>
       <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search"/>
+      <input type="text" id="search" onChange={handleChange} />
 
       <hr/>
 
-      <List />
+      <List list={stories} />
     </div>
   );
-
+}
 
 export default App;
