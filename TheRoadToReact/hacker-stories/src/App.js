318078647d836bd558b72ad93a506639e2886d8a
EricRicketts <eric_ricketts@me.com>
2021-06-01 07:31:42 -0400

    object destructuring in application

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const List = props =>
-  props.list.map(item => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.number_of_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  ));
+const List = ({ list }) => list.map(item => <Item key={item.objectID} item={item} />);
+
+const Item = ({ item }) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.number_of_comments}</span>
+    <span>{item.points}</span>
+  </div>
+);
 const Search = ({ search, onSearch }) => (
     <div>
       <label htmlFor="search">Search: </label>
