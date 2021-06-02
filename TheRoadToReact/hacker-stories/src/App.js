5d7ec4edbb4d9435d6af208193c4ebd9c022e9ea
EricRicketts <eric_ricketts@me.com>
2021-06-02 17:23:46 -0400

    custom hooks

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -25,6 +25,15 @@ const Search = ({ search, onSearch }) => (
       />
     </div>
   );
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
+  return [value, setValue];
+}
 const App = () => {
   const stories = [
     {
@@ -44,33 +53,9 @@ const App = () => {
       objectID: 1
     }
   ];
-  /*
-    It is very important to understand how state is being managed so far in this application.  We initialize the
-    searchTerm and the updating function, setSearchTerm, by use of React.useState().  We defined a function,
-    handleSearch at the App level which will actually update the searchTerm.  This is critical to understand, since
-    searchTerm is at the App level, it can be passed to any component by way of props.
-
-    This is the case with the filtering function searchedStories, it takes the searchTerm and uses that as the basis
-    for searching among the stories.
-
-    So how does the Search component actually set the updated value for searchTerm?  Note the Search component is
-    instantiated with a "onSearch" attribute, in the definition of the Search component, "onSearch" becomes a prop
-    which is passed the "handleSearch" function.  So where does the "handleSearch" function get access to the event
-    object?  From the onChange attribute in the input element defined within the Search component.  So once onChange
-    triggers an event object, props.onSearch is called which calls the handleSearch function, new the searchTerm
-    variable is updated at the App level.
-
-    The problem we had with the prior code is that if we changed the initial state the new initial value did not show
-    up as the value for the input element.  Furthermore, the filter would run and the updated state would be reflected
-    in the UI.  So for instance with 'React' as the new default value, only the React data would be shown but the
-    string 'React' would not be in the input field of the input element.  To fix this we add another prop to the
-    Search component which captures the searchTerm variable and through props passes the searchTerm variable to
-    the value attribute of the input element.
-  */
-  const [searchTerm, setSearchTerm] = React.useState('React');
-
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', '');
   const handleSearch = event => {
-    setSearchTerm(event.target.value)
+    setSearchTerm(event.target.value);
   }
   const searchedStories = stories.filter(story =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
