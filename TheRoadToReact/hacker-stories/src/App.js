498bff76a717df33d6135a6ee6164bd2f29aa577
EricRicketts <eric_ricketts@me.com>
2021-06-06 08:54:00 -0400

    conditional state

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -73,8 +73,29 @@ const App = () => {
       objectID: 1
     }
   ];
+
+  const getAsyncStories = () =>
+    new Promise(resolve =>
+      setTimeout(() =>
+        resolve({ data: { stories: initialStories } }),
+        2000
+      )
+    );
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', '');
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+
+    getAsyncStories()
+      .then(result => {
+      setStories(result.data.stories);
+      setIsLoading(false);
+    })
+      .catch(() => setIsError(true));
+  }, []);
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -100,7 +121,16 @@ const App = () => {
       </InputWithLabel>
       <hr/>
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+      {isError && <p>Something went wrong ...</p>}
+
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List
+          list={searchedStories}
+          onRemoveItem={handleRemoveStory}
+        />
+      )}
     </div>
   );
 }
