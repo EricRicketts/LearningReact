33cb09a3e3e8b3ac63a18a57dc0034bd7ce618f5
EricRicketts <eric_ricketts@me.com>
2021-06-08 13:48:33 -0400

    adding a form component

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -46,6 +46,25 @@ const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, ch
     </>
   );
   };
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case 'STORIES_FETCH_INIT':
@@ -97,8 +116,9 @@ const App = () => {
   const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
   }
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
   }
 
   const handleFetchStories = React.useCallback(async () => {
@@ -129,25 +149,15 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
+
       <hr/>
 
       {stories.isError && <p>Something went wrong ...</p>}
-
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
