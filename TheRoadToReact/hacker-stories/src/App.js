cd0874d805a9476acca98afc0456f00d103258c3
EricRicketts <eric_ricketts@me.com>
2021-06-08 11:01:22 -0400

    explicit data fetching

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -87,17 +87,24 @@ const useSemiPersistentState = (key, initialState) => {
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'react');
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
     { data: [], isLoading: false, isError: false }
   );
 
-  // React.useEffect(() => {
+  const handleSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  }
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+  }
+
   const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then(response => response.json())
       .then(result => {
         dispatchStories({
@@ -107,24 +114,18 @@ const App = () => {
     })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
-  }, [searchTerm]);
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
   }, [handleFetchStories]);
 
-  const handleSearch = event => {
-    setSearchTerm(event.target.value);
-  }
   const handleRemoveStory = item => {
     dispatchStories({
       type: 'REMOVE_STORY',
       payload: item
     });
   }
-  // const searchedStories = stories.data.filter(story =>
-  //   story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  // );
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -132,10 +133,18 @@ const App = () => {
       <InputWithLabel
         id="search"
         value={searchTerm}
-        onInputChange={handleSearch}
+        isFocused
+        onInputChange={handleSearchInput}
       >
         <strong>Search:</strong>
       </InputWithLabel>
+      <button
+        type="button"
+        disabled={!searchTerm}
+        onClick={handleSearchSubmit}
+      >
+        Submit
+      </button>
       <hr/>
 
       {stories.isError && <p>Something went wrong ...</p>}
