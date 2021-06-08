c12e81d21fb1756b68c2049449f07281d0db789b
EricRicketts <eric_ricketts@me.com>
2021-06-07 21:13:31 -0400

    memoized handlers

--- TheRoadToReact/hacker-stories/src/App.js
+++ TheRoadToReact/hacker-stories/src/App.js
@@ -92,7 +92,8 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
-  React.useEffect(() => {
+  // React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
@@ -107,6 +108,10 @@ const App = () => {
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
